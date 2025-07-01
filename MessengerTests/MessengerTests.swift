b51ef08fd99d14a2ae9d5f39ac77683ddc08c449
Mehak Noorani <mehak@miletap.com>
2025-07-01 11:01:33 +0500

    Initial Commit

---
+++ MessengerTests/MessengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTests.swift
+//  MessengerTests
+//
+//  Created by Workwise 1 on 01/07/2025.
+//
+
+import Testing
+@testable import Messenger
+
+struct MessengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
